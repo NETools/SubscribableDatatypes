49c095146830be1b55b72ba732c26eefb9d73c44
NETools <117414781+NETools@users.noreply.github.com>
2023-06-23 14:24:26 +0200

    Projektdateien hinzufügen.

---
+++ SubscribableDatatypes/NSVariable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscribableDatatypes
+{
+	internal class NSVariable<T> where T : notnull
+	{
+		private T _currentValue;
+		public T Value
+		{
+			get => _currentValue;
+			set
+			{
+				if (_currentValue.Equals(value))
+					return;
+
+				var old = _currentValue;
+				_currentValue = value;
+				ValueChanged?.Invoke(old);
+			}
+		}
+
+		public event Action<T>? ValueChanged;
+
+		public NSVariable(T initialValue)
+		{
+			_currentValue = initialValue;
+		}
+
+		public static implicit operator NSVariable<T>(T value)
+		{
+			return new NSVariable<T>(value);
+		}
+
+		public static implicit operator T(NSVariable<T> variable)
+		{
+			return variable.Value;
+		}
+	}
+}
---
+++ SubscribableDatatypes/Program.cs
@@ -0,0 +1,18 @@
+﻿// - 1 -
+using SubscribableDatatypes;
+using System.Diagnostics;
+
+Test test = new Test();
+
+test.IntegerVariable.ValueChanged += IntegerVariable_ValueChanged;
+test.IntegerVariable.Value = 5;
+
+void IntegerVariable_ValueChanged(int oldValue)
+{
+	Console.WriteLine($"Value of {nameof(test.IntegerVariable)} has changed from {oldValue} to {test.IntegerVariable.Value}.");
+}
+
+class Test
+{
+	public NSVariable<int> IntegerVariable { get; set; } = 15;
+}
